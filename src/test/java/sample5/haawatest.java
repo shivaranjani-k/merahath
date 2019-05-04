1306c52ae15f352ebe21f5d3fc733d11c4090754
csk <shivaranjani229@gmail.com>
2019-05-04 22:58:03 +0530

    push to gitbash

---
+++ src/test/java/sample5/haawatest.java
@@ -0,0 +1,14 @@
+package sample5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class haawatest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
